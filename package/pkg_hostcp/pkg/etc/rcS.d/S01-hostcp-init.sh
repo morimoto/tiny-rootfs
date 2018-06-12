0b1caafecf06392e3a08c388d34ea5ff6e018422
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2018-06-12 11:24:21 +0900

    hostcp: tidyup init for hostcp.config file
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- package/pkg_hostcp/pkg/etc/rcS.d/S01-hostcp-init.sh
+++ package/pkg_hostcp/pkg/etc/rcS.d/S01-hostcp-init.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 # create /etc/hostcp.conf which contains ip address to our bootserver
-dmesg | grep bootserver= | cut -c 17- | cut -d "," -f 1 > /etc/hostcp.conf
+dmesg | grep bootserver= | cut -d = -f2 | cut -d , -f1 > /etc/hostcp.conf
